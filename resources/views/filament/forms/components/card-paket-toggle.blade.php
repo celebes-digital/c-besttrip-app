800026ddb9769300ffd4dc0a1a863a833a442161
perdidev <perdidev@gmail.com>
2024-11-06 15:40:22 +0800

    refactor: cahange card color accent when choose paket to add jemaah

--- resources/views/filament/forms/components/card-paket-toggle.blade.php
+++ resources/views/filament/forms/components/card-paket-toggle.blade.php
@@ -6,22 +6,22 @@
         <div class="grid md:grid-col-2 lg:grid-cols-3 gap-4 w-full my-4">
             @foreach (\App\Models\Paket::whereBetween('tgl_paket', [$this->data['from_date'], $this->data['to_date']])->get()->toArray() as $item)
                 <div
-                    class="w-full space-y-2 border p-4 rounded-2xl {{ $this->data['paket_id'] === $item['id'] ? 'border-teal-400 bg-teal-100/20' : 'border-zinc-200' }}">
+                    class="w-full space-y-2 border p-4 rounded-2xl {{ $this->data['paket_id'] === $item['id'] ? 'border-rose-400 bg-rose-100/25' : 'border-zinc-200' }}">
                     <div class="w-full aspect-video bg-slate-200 rounded-xl"></div>
                     <div class="space-y-2">
                         <h1 class="font-semibold text-lg">Paket {{ $item['nama_paket'] }}</h1>
                         <p
-                            class="bg-teal-400/25 w-fit h-fit border border-teal-50 rounded-md px-2 py-1 font-medium text-teal-700">
+                            class="bg-rose-400/25 w-fit h-fit border border-rose-50 rounded-md px-2 py-1 font-medium text-rose-700">
                             {{ $item['tgl_paket'] }}</p>
                         <p class="line-clamp-2 text-base font-light">Lorem, ipsum dolor sit amet consectetur adipisicing
                             elit. Sint vel aliquid, culpa nam provident qui eveniet cumque rerum? Quam sint totam eaque
                             explicabo recusandae praesentium pariatur dolores animi commodi sunt.</p>
                         <div class="py-2">
-                            <span class="text-teal-500 font-semibold text-xl !py-12">Rp{{ $item['harga'] }}</span>
+                            <span class="text-rose-500 font-semibold text-xl !py-12">Rp{{ $item['harga'] }}</span>
                         </div>
                     </div>
                     <button wire:click="set('data.paket_id', {{ $item['id'] }})"
-                        class="bg-teal-600 hover:bg-teal-500 transition-all ease-in-out rounded-lg text-white w-full p-2">{{ $this->data['paket_id'] === $item['id'] ? 'Terpilih' : 'Pilih Paket' }}</button>
+                        class="bg-rose-600 hover:bg-rose-500 transition-all ease-in-out rounded-lg text-white w-full p-2">{{ $this->data['paket_id'] === $item['id'] ? 'Terpilih' : 'Pilih Paket' }}</button>
                 </div>
             @endforeach
         </div>
