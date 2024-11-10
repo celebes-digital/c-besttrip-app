d3da4026f40482f3849b9287a09da8f3edbb729f
perdidev <perdidev@gmail.com>
2024-11-10 21:46:33 +0800

    feat: tab in setoran jemaah by status setoran

--- app/Filament/Resources/SetoranJemaahResource/Pages/ListSetoranJemaahs.php
+++ app/Filament/Resources/SetoranJemaahResource/Pages/ListSetoranJemaahs.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\SetoranJemaahResource\Pages;
 
+use App\Enums\StatusSetoran;
 use App\Filament\Resources\SetoranJemaahResource;
+use App\Models\SetoranJemaah;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListSetoranJemaahs extends ListRecords
 {
@@ -14,6 +18,49 @@ class ListSetoranJemaahs extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all'           
+                => Tab::make(),
+                
+            'pending'       
+                => Tab::make()
+                ->icon(StatusSetoran::Pending->getIcon())
+                ->label(StatusSetoran::Pending->getLabel())
+                ->badgeColor(StatusSetoran::Pending->getColor())
+                ->badge(
+                    SetoranJemaah::query()->where('status_setoran', StatusSetoran::Pending)->count())
+                ->modifyQueryUsing(
+                    fn(Builder $query) 
+                    => $query->where('status_setoran', StatusSetoran::Pending)),
+
+            'terverifikasi' 
+                => Tab::make()
+                ->icon(StatusSetoran::Terverifikasi->getIcon())
+                ->label(StatusSetoran::Terverifikasi->getLabel())
+                ->badgeColor(StatusSetoran::Terverifikasi->getColor())
+                ->badge(
+                    SetoranJemaah::query()->where('status_setoran', StatusSetoran::Terverifikasi)->count()
+                )
+                ->modifyQueryUsing(
+                    fn(Builder $query) 
+                    => $query->where('status_setoran', StatusSetoran::Terverifikasi)),
+
+            'ditolak' 
+                => Tab::make()
+                ->icon(StatusSetoran::Ditolak->getIcon())
+                ->label(StatusSetoran::Ditolak->getLabel())
+                ->badgeColor(StatusSetoran::Pending->getColor())
+                ->badge(
+                    SetoranJemaah::query()->where('status_setoran', StatusSetoran::Ditolak)->count()
+                )
+                ->modifyQueryUsing(
+                    fn(Builder $query) 
+                    => $query->where('status_setoran', StatusSetoran::Ditolak)),
         ];
     }
 }
