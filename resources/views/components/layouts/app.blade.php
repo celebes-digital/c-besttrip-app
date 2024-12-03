33f3a949aa2713e01e1bf588aa04c2a3cb244283
perdidev <perdidev@gmail.com>
2024-12-03 11:41:59 +0800

    feat: enhance SEO and meta tags in app layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,38 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'C-Besttrip' }}</title>
+        <title>{{ $title ?? 'C-Besttrip - Celebes Group' }}</title>
+        <meta name="description" content="{{ $description ?? 'C-Besttrip - Celebes Group' }}">
+
+        {{-- Favicon --}}
+        <link rel="icon" href="{{ asset('logo.png') }}" type="image/png">
+
+        {{-- CSRF Token --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{-- SEO Meta --}}
+        <meta name="robots" content="index, follow">
+        <meta name="author" content="PT. Celebes Solusi Digital">
+        <meta name="keywords" content="C-Besttrip, Traveler, Travel Agency, Umrah, Umrah Agency, Travel, Travel Services, Umrah Services, Celebes Group, Travel Indonesia, Umrah Indonesia, Travel Makassar, Umrah Makassar">
+        <meta name="googlebot" content="index, follow">
+        <meta name="bingbot" content="index, follow">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="application-name" content="C-Besttrip">
+
+        {{-- Open Graph / Facebook --}}
+        <meta property="og:title" content="{{ $title ?? 'C-Besttrip - Celebes Group' }}">
+        <meta property="og:description" content="{{ $description ?? 'C-Besttrip - Celebes Group' }}">
+        <meta property="og:image" content="{{ asset('logo.png') }}">
+        <meta property="og:url" content="{{ url()->current() }}">
+        <meta property="og:type" content="website">
+
+        {{-- Twitter --}}
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{ $title ?? 'C-Besttrip - Celebes Group' }}">
+        <meta name="twitter:description" content="{{ $description ?? 'C-Besttrip - Celebes Group' }}">
+        <meta name="twitter:image" content="{{ asset('logo.png') }}">
+        <meta name="twitter:site" content="@your_twitter_handle">
+        <meta name="twitter:creator" content="@your_twitter_handle">
         
         {{-- Font --}}
         <link rel="preconnect" href="https://fonts.googleapis.com">
